7c89527526cd5770151a7205e6d647d942c7ced3
Madiha shah <162611745+madiha-sha@users.noreply.github.com>
2024-06-02 02:08:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//assignment 9
+//stor a favourite number
+var favouriteNumber = 7;
+var message = "my favourite number is ".concat(favouriteNumber);
+console.log(message);
+//assignment 10
+// two number of variable
+var numone = 7;
+var numtwo = 5;
+// add the two variable
+console.log(numone + numtwo);
---
+++ main.ts
@@ -0,0 +1,15 @@
+//assignment 9
+//stor a favourite number
+let favouriteNumber: number = 7;
+let message = `my favourite number is ${favouriteNumber}`;
+
+console.log(message);
+
+//assignment 10
+// two number declear as a variable
+let numone: number = 7
+let numtwo: number = 5
+// add the two variable
+
+console.log(numone + numtwo);
+
